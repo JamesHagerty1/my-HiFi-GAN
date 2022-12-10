e08bfce82d8ee680e6e7c132add5a58007e8681e
James <jhagerty@alumni.cmu.edu>
2022-12-10 10:19:51 -0500

    init G for training

--- generator_model.py
+++ generator_model.py
@@ -8,17 +8,14 @@ class Generator(torch.nn.Module):
     def __init__(self, config):
         super(Generator, self).__init__()
 
-        # Embedding(K_discrete_units, embedding_dim)
-        # x (batch_size, sequence_len) -> (batch_size, sequence_len, embedding_dim)
         self.lookup_table = nn.Embedding(config.num_embeddings, 
                                          config.embedding_dim)
 
-        # Conv1d(in_channels, out_channels, kernel_size, stride, padding)
-        # x (batch_size, in_channels, seq_len) -> (batch_size, out_channels, seq_len)
         conv1 = Conv1d(config.embedding_dim, config.upsample_initial_channel, 
                        7, 1, padding=3)
         self.conv_pre = weight_norm(conv1)
 
+        
     def forward(self, x):
         x = self.lookup_table(x)  
         x = x.transpose(1, 2)
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import json
 
+import torch
 from torch.utils.data import DataLoader
 
 from data import dataloader_init
@@ -7,22 +8,36 @@ from generator_model import Generator
 from utils import AttrDict
 
 
+EPOCHS = 3100
+
+
 def main():
     with open("hubert100_lut.json") as f:
         data = f.read()
     config = AttrDict(json.loads(data))
 
+    torch.manual_seed(config.seed)
+    torch.cuda.manual_seed(config.seed)
+    device = torch.device("cuda:0")
+
     dataloader = dataloader_init(config)
+    generator = Generator(config).to(device)
 
-    generator = Generator(config)
+    for epoch in range(EPOCHS):
+        for i, batch in enumerate(dataloader):
+            x, y, _, y_mel = batch
 
-    for i, batch in enumerate(dataloader):
-        x, y, _, y_mel = batch
-        
-        y_g_hat = generator(x)
-        print(y_g_hat.shape)
+            x = torch.autograd.Variable(x.to(device, non_blocking=True))
+            y = torch.autograd.Variable(y.to(device, non_blocking=True))
+            y_mel = torch.autograd.Variable(y_mel.to(device, non_blocking=True))
+            y = y.unsqueeze(1)
+            print("* ok")
 
-        break
+            y_g_hat = generator(x)
+         
+            break
+        break    
+    
 
 
 if __name__ == "__main__":
