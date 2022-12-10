7343b151a1a663027dda6238aafb77a62d2ad1c7
James <jhagerty@alumni.cmu.edu>
2022-12-10 11:39:32 -0500

    inference start

--- inference.py
+++ inference.py
@@ -1,12 +1,29 @@
+import json
 import torch
-import numpy
-from torch.nn import Conv1d
 
 from generator_model import Generator
 
+from utils import AttrDict
+
+
+CHECKPOINT_FILE = "checkpoints/testG0"
+TEST_FILE_DIR = "test_files/"
+
 
 def main():
-    pass
+    with open("hubert100_lut.json") as f:
+        data = f.read()
+    h = AttrDict(json.loads(data))
+
+    device = torch.device("cuda:0")
+    generator = Generator(h).to(device)
+    state_dict_g = torch.load(CHECKPOINT_FILE, map_location=device)
+    generator.load_state_dict(state_dict_g['generator'])
+
+    # generator.eval()
+    # generator.remove_weight_norm()
+    # with torch.no_grad():
+    #     pass
 
 
 if __name__ == "__main__":
